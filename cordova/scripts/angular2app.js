bf8936a24467b02e80a4b7c86e62972aa0e760c4
michatrautweiler <michatrautweiler@gmail.com>
2017-09-19 00:59:46 +0200

    android build

--- cordova/scripts/angular2app.js
+++ cordova/scripts/angular2app.js
@@ -7,10 +7,17 @@ module.exports = function(context) {
     const baseWWW = basePath + '/www';
 
     console.log(execSync(
-      "ng build --target=production --environment=prod --output-path cordova/www/",
+      "ng build --target=production --environment=prod --output-path cordova/www/ --base-href .",
       {
         maxBuffer: 1024*1024,
         cwd: basePath + '/..'
       }).toString('utf8')
     );
+	
+	var files = fs.readdirSync(baseWWW);
+    for (var i = 0; i < files.length; i++) {
+      if (files[i].endsWith('.gz')) {
+        fs.unlinkSync(baseWWW + '/' + files[i]);
+      }
+    }
 };
